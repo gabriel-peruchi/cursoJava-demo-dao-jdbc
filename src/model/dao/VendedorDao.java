70401c173177b807a8ed7257832a7df8684d7fa7
Gabriel Peruchi <gabriel-peruchi@hotmail.com>
2019-04-29 15:21:42 -0300

    DepartamentoDao e VendedorDao

---
+++ src/model/dao/VendedorDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entidades.Vendedor;
+
+public interface VendedorDao {
+	
+	void inserir(Vendedor obj);
+	void update(Vendedor obj);
+	void deletePorId(Integer id);
+	Vendedor consultaPorId(Integer id);
+	List<Vendedor> consultaTodos();
+}
